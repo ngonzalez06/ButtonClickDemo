098c2d7914d98146c5b2a6c06a82cd7b3ba7de33
ericsauber <eric.sauber@mavs.uta.edu>
2017-04-22 05:18:31 -0500

    changed

---
+++ app/src/main/java/com/example/ericsauber/buttonclickdemo/alsoActivity.java
@@ -0,0 +1,13 @@
+package com.example.ericsauber.buttonclickdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alsoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_also);
+    }
+}
